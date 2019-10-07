93a10f078b6cd2cbe88c571b80f038d67b199e47
GroovyEverywhere <55300195+GroovyEverywhere@users.noreply.github.com>
2019-10-07 08:44:04 -0700

    Create DessertItem.java

---
+++ Assignment-4/Q3/DessertItem.java
@@ -0,0 +1,21 @@
+package Assignment4;
+
+public abstract class DessertItem {
+	protected String name;
+	
+
+	public DessertItem()  {
+	
+	}
+
+	public DessertItem(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public abstract int getCost();
+	
+}
