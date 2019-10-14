3e18a7bfe36a698056c93a35d0ab97d534427577
GroovyEverywhere <55300195+GroovyEverywhere@users.noreply.github.com>
2019-10-14 00:57:51 -0700

    Create Boardable.java

---
+++ Assignment-5/Q5/Boardable.java
@@ -0,0 +1,26 @@
+package Boardable;
+
+public interface Boardable {
+
+	static void setBoardStart(int month, int day, int year){
+		if(month < 1 || month > 12 || day < 1 || day >31 || year <1000 || year >9999) {
+			return;
+		}
+	}
+	
+	static void setBoardEnd(int month, int day, int year) {
+		if(month < 1 || month > 12 || day < 1 || day >31 || year <1000 || year >9999) {
+			return;
+		}
+	}
+	
+	static boolean boarding(int month, int day, int year) {
+		if(month < 1 || month > 12 || day < 1 || day >31 || year <1000 || year >9999) {
+			return false;
+		}
+		else{
+			return true;
+		}
+		
+	}
+}
