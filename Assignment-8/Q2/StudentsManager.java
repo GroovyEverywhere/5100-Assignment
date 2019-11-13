c6317cc6cf88af5d8d628d2fd4c31ade1edf8db5
GroovyEverywhere <55300195+GroovyEverywhere@users.noreply.github.com>
2019-11-12 17:05:28 -0800

    Create StudentsManager.java

---
+++ Assignment-8/Q2/StudentsManager.java
@@ -0,0 +1,19 @@
+package Assignment8;
+
+import java.util.List;
+
+public interface StudentsManager {
+	public void addStudent(Student student) throws RuntimeException;
+
+	public void deleteStudent(String id);
+
+	public void modifyStudent(String id, Student student);
+
+	public Student findStudent(String name);
+
+	public int getStudentCount();
+
+	public void display();
+
+	public List<Student> getStudents();
+}
